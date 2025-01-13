1e34286960d809454b2737ac9abd9feaf21c75bc
Nathan Arakawa <125909386+generalheisenberg1982@users.noreply.github.com>
2025-01-13 08:24:15 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,93 +33,16 @@ double Voltage_Adjuster(double Input_Voltage, double Output_Voltage, double Moto
 	else{
 		return needed_voltage;}}
 
-int Chassis_Control( ) {
-	double leftstick = global::DriversInput.get_analog(E_CONTROLLER_ANALOG_LEFT_X);
-	double rightstick = global::DriversInput.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
-
-	global::RC.move_voltage(rightstick);
-	global::LC.move_voltage(rightstick);
-	Voltage_R.push_back(leftstick);
-	Voltage_L.push_back(rightstick);
-	if (leftstick >= 0){
-		global::RC.move_voltage(leftstick);
-		global::LC.move_voltage(leftstick * -1 );}
-	if (leftstick < 0 ){
-		global::LC.move_voltage(leftstick);
-		global::RC.move_voltage(leftstick * -1 );}}
-
-//Voltage_Adjuster(leftstick);
-
-void Lady_Brown_Control( ) {
-	if (global::DriversInput.get_digital(E_CONTROLLER_DIGITAL_L1)){
-		timestamp = pros::millis();
-		global::LadyBrown.move_voltage(127);
-		Voltage_LB.push_back(127);}
-		Voltage_LB_time.push_back(timestamp);
-	if (global::DriversInput.get_digital(E_CONTROLLER_DIGITAL_R1)){
-		timestamp = pros::millis();
-		global::LadyBrown.move_voltage(-127);
-		Voltage_In.push_back(-127);
-		Voltage_LB_time.push_back(timestamp);}
-	else {
-		Voltage_In.push_back(0);}}
-
-void Intake_Control( ) {
-	if (global::DriversInput.get_digital(E_CONTROLLER_DIGITAL_L2)){
-		timestamp = pros::millis();
-		global::Intake.move(127);
-		Voltage_In.push_back(127);
-		Voltage_In_time.push_back(timestamp);}
-	if (global::DriversInput.get_digital(E_CONTROLLER_DIGITAL_R2)){
-		timestamp = pros::millis();
-		global::Intake.move(-127);
-		Voltage_In.push_back(127);
-		Voltage_In_time.push_back(timestamp);}}
-
-void Clamp_Control( ){
-	timestamp = pros::millis();
-	//Voltage_C.push_back(127);
-	Voltage_C_time.push_back(timestamp);
-	//REMEBER TO DO THIS BY 1/7/25
-	}
-
-bool Volatage_Record(){
-	if (global::DriversInput.get_digital(E_CONTROLLER_DIGITAL_L2) && global::DriversInput.get_digital(E_CONTROLLER_DIGITAL_R2) && global::DriversInput.get_digital(E_CONTROLLER_DIGITAL_A)){
-		FILE* Voltage_L_file = fopen("/usd/Voltag_L.bin", "wb");
-		FILE* Voltage_R_file = fopen("/usd/Voltage_R.bin","wb");
-		FILE* Voltage_LB_file = fopen("/usd/Voltage_LB.bin", "wb");
-		FILE* Voltage_In_file = fopen("/usd/Voltage_In.bin", "wb");
-		FILE* Voltage_C_file = fopen("/usd/Voltage_C.bin","wb");
-		FILE* Voltage_LB_time_file = fopen("/usd/Voltage_LB_time.bin","wb");
-		FILE* Voltage_In_time_file = fopen("/usd/Voltage_In_time.bin","wb");
-		FILE* Voltage_C_time_file = fopen("/usd/Voltage_C_time.bin","wb");
-		if (!Voltage_L_file || !Voltage_R_file || !Voltage_LB_file || !Voltage_In_file || !Voltage_C_file || !Voltage_LB_time_file || !Voltage_In_time_file || !Voltage_C_time_file) {
-			DriversInput.clear();
-			DriversInput.print(0,1, "Error 1: Failed to open file.");
-		return;}
-		DriversInput.clear();
-		DriversInput.print(0,1, "Message 4: Files opened.");
-		DriversInput.print(0,2, "Message 1: Recording drive.");
-		int8_t Voltage_L; int8_t Voltage_R; int8_t Voltage_LB; int8_t Voltage_In; int8_t Voltage_C; int8_t Voltage_LB_time; int8_t Voltage_In_time; int8_t Voltage_C_time;
-		fwrite(&Voltage_L , sizeof(Voltage_L) , 1, Voltage_L_file );
-		fwrite(&Voltage_R , sizeof(Voltage_R) , 1, Voltage_R_file );
-		fwrite(&Voltage_LB, sizeof(Voltage_LB), 1, Voltage_LB_file);
-		fwrite(&Voltage_In, sizeof(Voltage_In), 1, Voltage_In_file);
-		fwrite(&Voltage_LB_time, sizeof(Voltage_LB_time), 1, Voltage_LB_time_file);
-		fwrite(&Voltage_In_time, sizeof(Voltage_In_time), 1, Voltage_In_time_file);
-		fwrite(&Voltage_C_time, sizeof(Voltage_C_time), 1, Voltage_C_time_file);
-		DriversInput.print(0,3, "Message 2: Drive recorded.");
-		fclose(Voltage_L_file);
-		fclose(Voltage_R_file);
-		fclose(Voltage_LB_file);
-		fclose(Voltage_In_file);
-		fclose(Voltage_C_file);
-		fclose(Voltage_LB_time_file);
-		fclose(Voltage_In_time_file);
-		fclose(Voltage_C_time_file);
-		DriversInput.print(0,4, "Message 3: Recording files closed.");
-		return recorded=true;}
-	else{}}
+void l(double lv){
+	global::L1.move_voltage(-lv);
+	global::L2.move_voltage(-lv);
+	global::L3.move_voltage(lv);
+}
+void r(double rv){
+	global::R1.move_voltage(-rv);
+	global::R2.move_voltage(-rv);
+	global::R3.move_voltage(rv);
+}
 
 std::vector<double> Voltage_L_read;
 std::vector<double> Voltage_R_read;
@@ -180,10 +103,10 @@ void Chassis_Playback(){
 	if (Voltage_L_read.empty()){}
 	else{
 		current_voltage = Voltage_L_read.front();
-		LC.move_voltage(current_voltage);
+		l(current_voltage);
 		Voltage_L_read.erase(Voltage_L_read.begin());
 		current_voltage = Voltage_R_read.front();
-		RC.move_voltage(current_voltage);
+		r(current_voltage);
 		Voltage_R_read.erase(Voltage_R.begin());}}
 
 void Lady_Brown_Playback(){
@@ -288,10 +211,11 @@ void autonomous() {Chassis_Playback();Lady_Brown_Playback();Intake_Playlist();Cl
  */
 void opcontrol() {
 	Program_Initialization();
+	Voltage_Playback_Data();
+	Chassis_Playback();
+	Intake_Playlist();
+	
 	while (true) {
-		Chassis_Control(); Intake_Control(); Lady_Brown_Control(); Clamp_Control();Volatage_Record();
-		if (recorded == true){
-			break;
-		}
+
 	}
 }
